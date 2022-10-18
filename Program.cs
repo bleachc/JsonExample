eba1dd788c3b2abeb20de4a605fef34695c44f42
Ben Leach <benleach@pm.me>
2022-10-18 19:02:48 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+
+namespace JsonExample;
+
+class Program
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> Pets { get; set; }
+    }
+
+    public static void Main(string[] args)
+    {
+        var collection = new List<Person>();
+        var ben = new Person
+        {
+            FirstName = "Ben",
+            LastName = "Leach",
+            Pets = new List<string>
+            {
+                "Winnie"
+            }
+        };
+        var bob = new Person
+        {
+            FirstName = "Bob",
+            LastName = "Noname",
+            Pets = new List<string>
+            {
+                "Spot"
+            }
+        };
+        collection.Add(ben);
+        collection.Add(bob);
+
+        var result = JsonSerializer.Serialize(collection);
+
+        Console.WriteLine(result);
+
+        var persons = JsonSerializer.Deserialize<List<Person>>(result);
+
+        foreach (var p in persons)
+        {
+            Console.WriteLine(p.FirstName);
+        }
+    }
+}
